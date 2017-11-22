4bb5370dff8b980550cb59573818d254c849773e
Lorenzo Manacorda <lorenzo@kinvolk.io>
2017-11-22 12:46:28 +0100

    Add template

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,30 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rocket_contrib;
+
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 
 use std::sync::atomic::{AtomicUsize, ATOMIC_USIZE_INIT, Ordering};
 
+use rocket_contrib::Template;
+
 static COUNTER: AtomicUsize = ATOMIC_USIZE_INIT;
 
+#[derive(Serialize)]
+struct TemplateContext {
+    count: usize,
+}
+
 #[get("/")]
-fn index() -> String {
+fn index() -> Template {
     COUNTER.fetch_add(1, Ordering::SeqCst);
 
-    format!("Hello, world! {}", COUNTER.load(Ordering::SeqCst))
+    let ctx = TemplateContext{ count: COUNTER.load(Ordering::SeqCst) };
+
+    Template::render("index", &ctx)
 }
 
 #[post("/")]
@@ -23,5 +37,6 @@ fn main() {
     rocket::ignite()
         .mount("/", routes![index])
         .mount("/reset", routes![reset])
+        .attach(Template::fairing())
         .launch();
 }
